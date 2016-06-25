e8d4e5bcc5d4a1d7f8c37ae739f4400bbd89639a
A92hm <ali92hm@gmail.com>
2016-06-24 18:01:23 -0700

    Passing installation path as a parameter

--- src/environment.js
+++ src/environment.js
@@ -4,15 +4,17 @@ const path = require('path')
 
 const utils = require('./utils')
 
+const installer = utils.getInstallerPath()
+
 /**
  * Uses PowerShell to configure the environment for
- * msvs_version 2015
+ * msvs_version 2015 and npm python 2.7
  *
  * @returns {Promise}
  */
 function setEnvironment () {
   const scriptPath = path.join(__dirname, '..', 'ps1', 'set-environment.ps1')
-  const psArgs = `& {& '${scriptPath}'}`
+  const psArgs = `& {& '${scriptPath}' -path '${installer.directory}' }`
   const args = ['-NoProfile', '-NoLogo', psArgs]
 
   return utils.executeChildProcess('powershell.exe', args)
