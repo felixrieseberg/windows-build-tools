759ed0e8657096ade1c88ee6722f4348e3d7e6e0
Felix Rieseberg <felix@felixrieseberg.com>
2016-06-29 17:53:37 +0200

    :memo: For shame, whitespace

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const download = require('./download')
 const install = require('./install')
 const environment = require('./environment')
 
-// Here lie the bodies of promises, killed in 
+// Here lie the bodies of promises, killed in
 // the name of better garbage collection
 download(function () {
   install(function (variables) {
