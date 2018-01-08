896a60c363c031968fa1ae6c5274a57737f5d910
jvmahon <jvm33@columbia.edu>
2018-01-08 18:39:10 -0500

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -6,21 +6,24 @@ var buildTools = {
   logName: 'build-tools-log.txt'
 }
 
-var python = {
-  installerName: 'python-2.7.14.msi',
-  installerUrl: pythonMirror.replace(/\/*$/, '/2.7.14/python-2.7.14.msi'),
-  targetName: 'python27',
-  logName: 'python-log.txt'
-}
-
-if (arch == "x64") {
+if (arch == "x64")
+{
   var python = {
   installerName: 'python-2.7.14.amd64.msi',
   installerUrl: pythonMirror.replace(/\/*$/, '/2.7.14/python-2.7.14.amd64.msi'),
   targetName: 'python27',
   logName: 'python-log.txt'
   }
+} else
+{
+  var python = {
+  installerName: 'python-2.7.14.msi',
+  installerUrl: pythonMirror.replace(/\/*$/, '/2.7.14/python-2.7.14.msi'),
+  targetName: 'python27',
+  logName: 'python-log.txt'
+  }
 }
+
 
 module.exports = {
   buildTools,
