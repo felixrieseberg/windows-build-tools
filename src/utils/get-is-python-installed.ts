a905afc6ce3e5812d78577de628192b073237119
Felix Rieseberg <felix@felixrieseberg.com>
2018-05-29 15:11:17 +0200

    :wrench: Cache Python installation result

--- src/utils/get-is-python-installed.ts
+++ src/utils/get-is-python-installed.ts
@@ -1,17 +1,21 @@
 import { spawnSync } from 'child_process';
 
+let _isPythonInstalled: string | null | undefined;
+
 export function getIsPythonInstalled() {
+  if (_isPythonInstalled !== undefined) return _isPythonInstalled;
+
   try {
     const options = { windowsHide: true, stdio: null };
     const { output } = spawnSync('python', [ '-V' ], options as any);
     const version = output.toString().trim().replace(/,/g, '');
 
     if (version && version.includes(' 2.')) {
-      return version;
+      return _isPythonInstalled = version;
     } else {
-      return null;
+      return _isPythonInstalled = null;
     }
   } catch (error) {
-    return null;
+    return _isPythonInstalled = null;
   }
 }
