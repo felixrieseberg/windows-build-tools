77028a7cddf59119b6ffa5eca541d8df445086d6
Felix Rieseberg <felix@felixrieseberg.com>
2018-05-29 15:10:41 +0200

    :wrench: Clean up the installer script

--- ps1/launch-installer.ps1
+++ ps1/launch-installer.ps1
@@ -1,73 +1,69 @@
 [CmdletBinding()]
 Param(
-    [Parameter(Mandatory=$True)]
-    [string]$path,
-    [string]$extraBuildToolsParameters,
-    [string]$pythonInstaller,
-    [string]$visualStudioVersion
+  [string]$BuildToolsInstallerPath,
+  [string]$ExtraBuildToolsParameters,
+  [string]$PythonInstaller,
+  [string]$VisualStudioVersion,
+  [switch]$InstallPython,
+  [switch]$InstallBuildTools
 )
 
 # Returns whether or not the current user has administrative privileges
-function IsAdministrator
-{
-    $Identity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
-    $Principal = New-Object System.Security.Principal.WindowsPrincipal($Identity)
-    $Principal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
+function IsAdministrator {
+  $Identity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+  $Principal = New-Object System.Security.Principal.WindowsPrincipal($Identity)
+  $Principal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
 }
 
 # Returns whether or not UAC is enabled on Windows
-function IsUacEnabled
-{
-    (Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System).EnableLua -ne 0
+function IsUacEnabled {
+  (Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System).EnableLua -ne 0
 }
 
 # Runs the installer
-function runInstaller
-{
-    if (Test-Path $path)
-    {
-        $extraParams = $extraBuildToolsParameters -split "%_; "
+function runInstaller {
+  if (Test-Path $BuildToolsInstallerPath) {
+    $extraParams = $ExtraBuildToolsParameters -split "%_; "
 
-        if ($extraParams.count -gt 0)
-        {
-            foreach ($element in $extraParams)
-            {
-                $params += $element
-            }
-        }
-
-        cd $path
-
-        if ($visualStudioVersion -eq "2017")
-        {
-            $params = "--norestart", "--quiet", "--add", "Microsoft.VisualStudio.Workload.VCTools"
-            ./vs_BuildTools.exe $params
-        }
-        else
-        {
-            $params = "/NoRestart", "/S", "/L", "`"$path\build-tools-log.txt`""
-            ./BuildTools_Full.exe $params
-        }
+    if ($extraParams.count -gt 0) {
+      foreach ($element in $extraParams) {
+        $params += $element
+      }
     }
+
+    cd $BuildToolsInstallerPath
+
+    if ($VisualStudioVersion -eq "2017") {
+      $params = "--norestart", "--quiet", "--add", "Microsoft.VisualStudio.Workload.VCTools"
+      ./vs_BuildTools.exe $params
+    }
+    else {
+      $params = "/NoRestart", "/S", "/L", "`"$BuildToolsInstallerPath\build-tools-log.txt`""
+      ./BuildTools_Full.exe $params
+    }
+  }
 }
 
-function runPythonInstaller
-{
-    if (Test-Path $path -and Test-Path $pythonInstaller)
-    {
-        cd $path
-        $pyParams = "/i", $pythonInstaller, "TARGETDIR=```"$path\python27```"", "ALLUSERS=0", "/qn", "/L*P", "`"$path\python-log.txt`""
-        Invoke-Expression "msiexec.exe $pyParams"
-    }
+function runPythonInstaller {
+  if (Test-Path $BuildToolsInstallerPath -and Test-Path $PythonInstaller) {
+    cd $BuildToolsInstallerPath
+    $pyParams = "/i", $PythonInstaller, "TARGETDIR=```"$BuildToolsInstallerPath\python27```"", "ALLUSERS=0", "/qn", "/L*P", "`"$BuildToolsInstallerPath\python-log.txt`""
+    Invoke-Expression "msiexec.exe $pyParams"
+  }
 }
 
 # Check Elevation
-if (!(IsAdministrator))
-{
-    "Please restart this script from an administrative PowerShell!"
-    "We cannot install the build tools without administrative rights."
-    return
+if (!(IsAdministrator)) {
+  "Please restart this script from an administrative PowerShell!"
+  "We cannot install the build tools without administrative rights."
+  return
 }
 
-runInstaller;
-runPythonInstaller;
+
+if ($InstallBuildTools.IsPresent) {
+  runInstaller;
+}
+
+if ($InstallPython.IsPresent) {
+  runPythonInstaller;
+}
